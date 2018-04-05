9d10ba6c4565c17c4c75c86ea7b87a2424bd9acb
sbx_user1059 <sbx_user1059@ip-10-12-201-34.eu-west-2.compute.internal>
2018-04-05 11:28:22 +0000

    Thu Apr 05 11:28:22 UTC 2018

--- test/solutions/checkout.test.js
+++ test/solutions/checkout.test.js
@@ -9,4 +9,8 @@ describe('basket', () => {
   it('should return -1 on bad input', () => {
     expect(calculate_value('ABCDm')).to.equal(-1)
   })
+
+  it('should calculate the value of items in a basket when there are no promotions', () => {
+    expect(calculate_value('ADBCDD')).to.equal(145)
+  })
 })
