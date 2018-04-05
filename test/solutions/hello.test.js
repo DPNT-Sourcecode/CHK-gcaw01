2eba9600b98eda1c36a014565a3d1f3ad823501b
sbx_user1059 <sbx_user1059@ip-10-12-201-34.eu-west-2.compute.internal>
2018-04-05 11:03:25 +0000

    Thu Apr 05 11:03:25 UTC 2018

---
+++ test/solutions/hello.test.js
@@ -0,0 +1,8 @@
+const hello = require('../../lib/solutions/hello');
+const expect = require('chai').expect;
+
+describe('sum', () => {
+  it('should return hello', () => {
+    expect(hello()).to.equal('Hello')
+  })
+})
