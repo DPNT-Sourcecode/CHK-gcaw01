1d5414f0bb19d75764df44898bf5bd3619633404
sbx_user1059 <sbx_user1059@ip-10-12-201-34.eu-west-2.compute.internal>
2018-04-05 11:16:22 +0000

    Thu Apr 05 11:16:22 UTC 2018

--- test/solutions/checkout.test.js
+++ test/solutions/checkout.test.js
@@ -1,7 +1,7 @@
 const calculate_value = require('../../lib/solutions/checkout');
 const expect = require('chai').expect;
 
-describe('hello', () => {
+describe('basket', () => {
   it('should return 0 for an empty basket', () => {
     expect(calculate_value('')).to.equal(0)
   })
