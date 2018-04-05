3154b474e57ba442110cb4982741acb780633390
sbx_user1059 <sbx_user1059@ip-10-12-201-34.eu-west-2.compute.internal>
2018-04-05 13:17:19 +0000

    Thu Apr 05 13:17:19 UTC 2018

--- test/solutions/checkout.test.js
+++ test/solutions/checkout.test.js
@@ -8,6 +8,7 @@ describe('basket', () => {
 
   it('should return -1 on bad input', () => {
     expect(calculate_value('ABCDm')).to.equal(-1)
+    expect(calculate_value('ABCd')).to.equal(-1)
   })
 
   it('should calculate the value of items in a basket when there are no promotions', () => {
@@ -16,11 +17,13 @@ describe('basket', () => {
 
   it('should take into account promotions', () => {
     expect(calculate_value('AAA')).to.equal(130)
+    expect(calculate_value('BB')).to.equal(45)
     expect(calculate_value('ABAA')).to.equal(160)
     expect(calculate_value('ABAACCC')).to.equal(220)
   })
 
   it('should take into account multiple promotions', () => {
-    // expect(calculate_value('ABABA')).to.equal(220)
+    expect(calculate_value('AAABB')).to.equal(175)
+    expect(calculate_value('ABCAAB')).to.equal(195)
   })
 })
