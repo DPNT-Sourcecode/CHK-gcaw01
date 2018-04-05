0c2278c67de8bccd86e9437b8beab8382865e1e0
sbx_user1059 <sbx_user1059@ip-10-12-201-34.eu-west-2.compute.internal>
2018-04-05 10:41:39 +0000

    Thu Apr 05 10:41:39 UTC 2018

--- test/solutions/test-sum.js
+++ test/solutions/test-sum.js
@@ -1,6 +1,12 @@
-var sum = require('../../lib/solutions/sum');
+const sum = require('../../lib/solutions/sum');
+const expect = require('chai').expect;
 
-exports['compute sum'] = function (test) {
-    test.equal(sum(1, 2), 3);
-    test.done();
-};
+describe('sum', () => {
+  it('should know 1+1 = 2', () => {
+    expect(sum(1,1)).to.equal(2)
+  })
+
+  it('should throw an exception when passed in numbers larger than 100', () => {
+      expect(sum(2,101)).to.throw()
+  })
+})
