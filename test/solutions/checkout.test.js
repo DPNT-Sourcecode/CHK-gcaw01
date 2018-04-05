2b25cd469deb75ba668da3286e9d1c78cfa45f0c
sbx_user1059 <sbx_user1059@ip-10-12-201-34.eu-west-2.compute.internal>
2018-04-05 11:19:22 +0000

    Thu Apr 05 11:19:22 UTC 2018

--- test/solutions/checkout.test.js
+++ test/solutions/checkout.test.js
@@ -5,4 +5,8 @@ describe('basket', () => {
   it('should return 0 for an empty basket', () => {
     expect(calculate_value('')).to.equal(0)
   })
+
+  it('should return -1 on bad input', () => {
+    expect(calculate_value('ABCDm')).to.return(-1)
+  })
 })
